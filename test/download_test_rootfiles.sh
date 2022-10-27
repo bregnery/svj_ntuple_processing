b77fcfc4b8b33ca4e519ddc0d3bde8c8c5e5361a
Thomas Klijnsma <klijnsma@fnal.gov>
2022-10-27 15:16:38 -0500

    initial commit

---
+++ test/download_test_rootfiles.sh
@@ -0,0 +1,3 @@
+xrdcp root://cmseos.fnal.gov//store/user/klijnsma/package_test_files/svj_ntuple_processing/madpt300_mz350_mdark10_rinv0.3.root .
+xrdcp root://cmseos.fnal.gov//store/user/klijnsma/package_test_files/svj_ntuple_processing/qcd_pt_800_1000.root .
+xrdcp root://cmseos.fnal.gov//store/user/klijnsma/package_test_files/svj_ntuple_processing/ttjets_inclusive.root .

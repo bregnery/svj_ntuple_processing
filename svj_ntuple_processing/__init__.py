2e65601b47e259e1033b7f4c8faf9d351c7c1786
snabili <snabili@umd.edu>
2023-05-02 16:45:57 -0400

    add metdphi cut to preselection

--- svj_ntuple_processing/__init__.py
+++ svj_ntuple_processing/__init__.py
@@ -462,6 +462,11 @@ def filter_preselection(array):
     a = a[dead_cell_mask]
     cutflow['ecaldeadcells'] = len(a)
 
+    # abs(metdphi)<1.5
+    metdphi = calc_dphi(arr['JetsAK15.fCoordinates.fPhi'][:,1].to_numpy(), arr['METPhi'].to_numpy())
+    a = a[metdphi<1.5]
+    cutflow['abs(metdphi)<1.5'] = len(a)
+
     cutflow['preselection'] = len(a)
 
     copy.array = a

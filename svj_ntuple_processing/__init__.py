7419c8de0cde7447fd9a390ad221733e959fbd95
snabili <snabili@umd.edu>
2023-05-15 22:45:06 -0400

    hardcode data weight

--- svj_ntuple_processing/__init__.py
+++ svj_ntuple_processing/__init__.py
@@ -901,7 +901,7 @@ def bdt_feature_columns(array, load_mc=True):
     a['ecfn2b2'] = arr['JetsAK15_ecfN2b2'][:,1].to_numpy()
     a['metdphi'] = calc_dphi(arr['JetsAK15.fCoordinates.fPhi'][:,1].to_numpy(), arr['METPhi'].to_numpy())
 
-    if load_mc: a['weight'] = arr['Weight'].to_numpy() #if 'Weight' in arr else np.ones(len(arr))
+    #if load_mc: a['weight'] = arr['Weight'].to_numpy() #if 'Weight' in arr else np.ones(len(arr))
     else: a['weight'] =np.ones(len(arr))
     a['met'] = arr['MET'].to_numpy()
     a['metphi'] = arr['METPhi'].to_numpy()

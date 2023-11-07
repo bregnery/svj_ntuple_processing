16141cec20fead7c35a8e8c518e0480520601058
Thomas Klijnsma <tklijnsm@gmail.com>
2023-11-07 15:27:33 -0600

    get only leading muon, write -1 if no muon

--- svj_ntuple_processing/__init__.py
+++ svj_ntuple_processing/__init__.py
@@ -1302,7 +1302,7 @@ def bdt_feature_columns(array, load_mc=True, save_scale_weights=False):
     # QCD high MET events 
     #a['CaloMET']        = arr['CaloMET'].to_numpy()
     #a['PFCaloMETRatio'] = arr['PFCaloMETRatio'].to_numpy()
-    a['lead_muonpt']    = arr['Muons.fCoordinates.fPt'].to_numpy()
+    a['lead_muonpt']    = ak.fill_none(ak.firsts(arr['Muons.fCoordinates.fPt']), -1.).to_numpy()
     a['nmuons']         = arr['NMuons'].to_numpy()
     a['nelectrons']     = arr['NElectrons'].to_numpy()
     '''a['lead_muoneta']   = arr['Muons.fCoordinates.fEta'][:,0].to_numpy()
